754328124effe032512cc10c7aaca2c28736c35b
WebMats <lorlando127@gmail.com>
2019-02-21 18:51:08 -0500

    added all GET routes

--- index.js
+++ index.js
@@ -2,9 +2,56 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+const recipesDB = require('./data/recipeBk');
+
 
 app.use(helmet());
 app.use(express.json());
+
+app.get('/dishes', async (req, res, next) => {
+    try {
+        const dishes = await recipesDB.getDishes();
+        res.status(200).json(dishes)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({errorMessage: "Could not fetch the dishes."})
+    }
+})
+app.get('/dishes/:id', async (req, res, next) => {
+    try {
+        const dish = await recipesDB.getDish(req.params.id);
+        if (!dish) {
+            res.status(404).json({errorMessage: "The dish with that id does not exist."})
+        } else {
+            res.status(200).json(dish);
+        }
+    } catch (err) {
+        console.log(err)
+        res.status({errorMessage: "Could not fetch at dish"})
+    }
+})
+app.get('/recipes', async (req, res, next) => {
+    try {
+        const recipes = await recipesDB.getRecipes();
+        res.status(200).json(recipes)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({errorMessage: "Could not fetch the recipes."})
+    }
+})
+app.get('/recipes/:id', async (req, res, next) => {
+    try {
+        const recipe = await recipesDB.getRecipe(req.params.id);
+        if (!recipe.name) {
+            res.status(404).json({errorMessage: "Could not find the recipe for that id."})
+        } else {
+            res.status(200).json(recipe);
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({errorMessage: "Could not fetch that recipe"})
+    }
+})
 
 
 
